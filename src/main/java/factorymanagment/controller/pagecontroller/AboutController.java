3391b3933f6246424d931fa4e506aea424e2533c
miauvo <aglaophot@gmail.com>
2018-07-24 00:07:28 +0300

    about change

--- src/main/java/factorymanagment/controller/pagecontroller/AboutController.java
+++ src/main/java/factorymanagment/controller/pagecontroller/AboutController.java
@@ -1,16 +1,17 @@
 package factorymanagment.controller.pagecontroller;
 
+import factorymanagment.model.domain.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class IndexController {
+public class AboutController {
 
     @GetMapping("/")
     public ModelAndView mainPage(){
         ModelAndView modelAndView = new ModelAndView("sds");
 
-        return new ModelAndView("home");
+        return new ModelAndView("about");
     }
 }
