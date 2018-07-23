b3106fc3b997d8ca7c024caed86993ff6c29560f
hhlTer <grzc@ukr.net>
2018-07-24 00:24:07 +0300

    test home page

---
+++ src/main/java/factorymanagment/service/jpa/EmployeeService.java
@@ -0,0 +1,20 @@
+package factorymanagment.service.jpa;
+
+import factorymanagment.model.domain.Employee;
+import factorymanagment.service.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmpoloyee(){
+        return employeeRepository.findAll();
+    }
+}
